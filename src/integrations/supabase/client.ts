5fdbcdf0dbf39438a3118d78caa22724ef479b84
DanielRivni <drivni123@gmail.com>
2025-12-01 13:45:45 +0200

    Changed the structure to fit Loveable

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,16 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = "https://xnxuqojzbfvlppsadwoa.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhueHVxb2p6YmZ2bHBwc2Fkd29hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1MzY0NTQsImV4cCI6MjA1NzExMjQ1NH0.x8OqRcZKoOqS0V17j74Cu95PeEnnh9q0ubQCMTypnpI";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
-  auth: {
-    storage: localStorage,
-    persistSession: true,
-    autoRefreshToken: true,
-  }
-});
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
